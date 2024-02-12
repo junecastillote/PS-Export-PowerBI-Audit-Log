751654c23891b1a3ea0608395aeccc36eb2e6407
June Castillote <june.castillote@gmail.com>
2024-02-12 15:59:09 +0800

    Update Get-PowerBIAuditLog.ps1

--- Get-PowerBIAuditLog.ps1
+++ Get-PowerBIAuditLog.ps1
@@ -48,79 +48,123 @@ param (
 
     [Parameter()]
     [DateTime]
-    $EndDate = (Get-Date)
+    $EndDate = (Get-Date),
+
+    [Parameter()]
+    [string]
+    $ExportFileName,
+
+    [Parameter()]
+    [bool]
+    $ReturnResult = $true
 )
 
-Begin {
-    ## Set progress bar visibility
-    $ProgressPreference = 'Continue'
 
-    ## Set progress bar style if PowerShell Core
-    if ($PSVersionTable.PSEdition -eq 'Core') {
-        $PSStyle.Progress.View = 'Classic'
-    }
+## Set progress bar visibility
+$ProgressPreference = 'Continue'
 
-    #Region - Is Exchange Connected?
+## Set progress bar style if PowerShell Core
+if ($PSVersionTable.PSEdition -eq 'Core') {
+    $PSStyle.Progress.View = 'Classic'
+}
+
+#Region - Is Exchange Connected?
+try {
+    $null = (Get-OrganizationConfig -ErrorAction STOP).DisplayName
+}
+catch [System.Management.Automation.CommandNotFoundException] {
+    "It looks like you forgot to connect to Remote Exchange PowerShell. You should do that first before asking me to do stuff for you." | Out-Default
+    $LASTEXITCODE = 1
+    Return $null
+}
+catch {
+    "Something is wrong. You can see the error below. You should fix it before asking me to try again." | Out-Default
+    $_.Exception.Message | Out-Default
+    $LASTEXITCODE = 1
+    Return $null
+}
+#EndRegion
+
+#Region ExtractPBILogs
+Function ExtractPBILogs {
+    Search-UnifiedAuditLog -SessionId $script:sessionID -SessionCommand ReturnLargeSet -StartDate $startDate -EndDate $endDate -Formatted -RecordType $script:recordType
+}
+$script:retryCount = 0
+#EndRegion
+
+"Start Date: $($StartDate)" | Out-Default
+"End Date: $($EndDate)" | Out-Default
+
+if ($StartDate -eq $EndDate) {
+    "The StartDate and EndDate cannot be the same values." | Out-Default
+    $LASTEXITCODE = 2
+    return $null
+}
+
+if ($EndDate -le $StartDate) {
+    "The EndDate value cannot be older than the StartDate value." | Out-Default
+    $LASTEXITCODE = 2
+    return $null
+}
+
+## Define the session ID and record type to use with the Search-UnifiedAuditLog cmdlet.
+$script:sessionID = (New-Guid).GUID
+$script:recordType = 'PowerBIAudit'
+
+## Create or overwrite the export file.
+if ($ExportFileName) {
     try {
-        $null = (Get-OrganizationConfig -ErrorAction STOP).DisplayName
-    }
-    catch [System.Management.Automation.CommandNotFoundException] {
-        "It looks like you forgot to connect to Remote Exchange PowerShell. You should do that first before asking me to do stuff for you." | Out-Default
-        Continue
+        $null = New-Item -ItemType File -Force -Path $ExportFileName -ErrorAction Stop
     }
     catch {
-        "Something is wrong. You can see the error below. You should fix it before asking me to try again." | Out-Default
+        ## If the export file cannot be created, exit the script.
         $_.Exception.Message | Out-Default
-        Continue
+        $LASTEXITCODE = 3
+        return $null
     }
-    #EndRegion
-
-    "Start Date: $($StartDate)" | Out-Default
-    "End Date: $($EndDate)" | Out-Default
-
-    if ($StartDate -eq $EndDate) {
-        "The StartDate and EndDate cannot be the same values." | Out-Default
-        Continue
-    }
-
-    if ($EndDate -le $StartDate) {
-        "The EndDate value cannot be older than the StartDate value." | Out-Default
-        Continue
-    }
-
-    ## Define the session ID and record type to use with the Search-UnifiedAuditLog cmdlet.
-    $script:sessionID = (New-Guid).GUID
-    $script:recordType = 'PowerBIAudit'
 }
 
-process {
-    do {
-        ## Run the Search-UnifiedAuditLog
-        $currentPageResult = Search-UnifiedAuditLog -SessionId $script:sessionID -SessionCommand ReturnLargeSet -StartDate $startDate -EndDate $endDate -Formatted -RecordType $script:recordType
-        if ($currentPageResult) {
-            ## Initialize the maximum results available variable once.
-            if (!$maxResultCount) {
-                $maxResultCount = $($currentPageResult[-1].ResultCount)
-                "Total entries: $($maxResultCount)" | Out-Default
-            }
-            ## Set the current page result count.
-            $currentPageResultCount = $($currentPageResult[-1].ResultIndex)
-            ## Compute the completion percentage
-            $percentComplete = ($currentPageResultCount * 100) / $maxResultCount
-            ## Display the progress
-            Write-Progress -Activity "Getting Power BI Audit Log [$($StartDate) - $($EndDate)]..." -Status "Result: $($currentPageResultCount) of $($maxResultCount)" -PercentComplete $percentComplete -ErrorAction SilentlyContinue
-            ## Display the current page results
+
+Write-Progress -Activity "Getting Power BI Audit Log [$($StartDate) - $($EndDate)]..." -Status "Progress: Getting the first 100 records (0%)" -PercentComplete 0 -ErrorAction SilentlyContinue
+do {
+    $currentPageResult = @(ExtractPBILogs)
+    if ($currentPageResult) {
+        ## Initialize the maximum results available variable once.
+        if (!$maxResultCount) {
+            $maxResultCount = $($currentPageResult[-1].ResultCount)
+            "Total entries: $($maxResultCount)" | Out-Default
+        }
+        ## Set the current page result count.
+        $currentPageResultCount = $($currentPageResult[-1].ResultIndex)
+        ## Compute the completion percentage
+        $percentComplete = ($currentPageResultCount * 100) / $maxResultCount
+        ## Display the progress
+        Write-Progress -Activity "Getting Power BI Audit Log [$($StartDate) - $($EndDate)]..." -Status "Progress: $($currentPageResultCount) of $($maxResultCount) ($([int]$percentComplete)%)" -PercentComplete $percentComplete -ErrorAction SilentlyContinue
+        ## Display the current page results
+        if ($ReturnResult) {
             $currentPageResult
         }
-    } while (
-        ## Continue running while the last ResultIndex in the current page is less than the ResultCount value.
-        ## Note: "ResultIndex" is not ZERO-based.
-        $currentPageResultCount -lt $maxResultCount
-    )
+        ## Export result to file
+        if ($ExportFileName) {
+            $currentPageResult | Export-Csv -Path $ExportFileName -Append
+        }
+    }
 }
+while (
+    ## Continue running while the last ResultIndex in the current page is less than the ResultCount value.
+    ## Note: "ResultIndex" is not ZERO-based.
+        ($currentPageResultCount -lt $maxResultCount) -or ($currentPageResult.Count -gt 0)
+)
 
-end {}
 
+Write-Progress -Activity "Getting Power BI Audit Log [$($StartDate) - $($EndDate)]..." -Status "Progress: $($currentPageResultCount) of $($maxResultCount) ($([int]$percentComplete)%)" -PercentComplete $percentComplete -ErrorAction SilentlyContinue -Completed
 
-
-
+if ($ExportFileName) {
+    $csv_file = Get-ChildItem -Path $ExportFileName
+    $zip_filename = $(($csv_file.FullName.ToString()).Replace($(((Split-Path $csv_file.FullName -Leaf).Split('.')[-1])), 'zip'))
+    $null = Compress-Archive -Path $ExportFileName -DestinationPath $zip_filename -CompressionLevel Optimal -Force
+    Start-Sleep -Seconds 2
+    $zip_file = Get-ChildItem -Path $zip_filename
+    "CSV result: $($csv_file.FullName)" | Out-Default
+    "ZIP result: $($zip_file.FullName)" | Out-Default
+}
